d51327db0a249c01838a852b69039f17143b6947
Yiqiao Yin <eagle0504@gmail.com>
2024-02-10 11:42:08 -0500

    ref added

--- app.py
+++ app.py
@@ -198,6 +198,7 @@ def main():
         output = rag(query=query, retrieved_documents=retrieved_documents)
         st.write(output)
         st.success("Please see where the chatbot got the information from the document below.👇")
+        st.write(results)
         st.table(results_as_table)
 
 
